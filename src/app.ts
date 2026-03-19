696503ba47529104920e3279a6734ed1778e3e1c
nitin-580 <iitknitin06@gmail.com>
2026-03-19 15:04:28 +0530

    added CORS PATCH/DELETE support and refined deployment

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ const app = express();
 app.use(helmet());
 app.use(cors({
   origin: env.CORS_ORIGIN,
-  methods: ['GET', 'POST', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
 }));
 
 // Body parsing Middleware
